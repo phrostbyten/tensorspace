7c8bd29426ed1f25e2c4a157e65e2f7cbc8ef761
JJ Ben-Joseph <opensource@phrostbyte.com>
2018-06-13 13:28:46 -0400

    Change logging level.

--- tensorspace/util.py
+++ tensorspace/util.py
@@ -3,24 +3,24 @@ import sys
 from pathlib import Path
 import os
 
-PROGRESS = 25
+STATUS = 25
 
-logging.basicConfig(stream=sys.stdout, level=PROGRESS)
-logging.addLevelName(PROGRESS, "PROGRESS")
+logging.basicConfig(stream=sys.stdout, level=STATUS)
+logging.addLevelName(STATUS, "STATUS")
 pb_logger = logging.getLogger('tensorspace')
 
 def log(msg):
-    pb_logger.log(PROGRESS, msg)
+    pb_logger.log(STATUS, msg)
 
 def get_data_home():
-    home = os.environ.get('PBHOME') or Path.home() / '.tensorspace'
+    home = os.environ.get('PBHOME') or Path.home() / '.phrostbyte'
     home = home / 'data'
     if not home.is_dir():
         Path.mkdir(home, parents=True)
     return home
 
 def get_vectors_home():
-    home = os.environ.get('PBHOME') or Path.home() / '.tensorspace'
+    home = os.environ.get('PBHOME') or Path.home() / '.phrostbyte'
     home = home / 'vectors'
     if not home.is_dir():
         Path.mkdir(home, parents=True)
