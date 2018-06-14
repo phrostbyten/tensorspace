1b27eb1ab99970ef2364f1fcf2b7b96d069c7599
JJ Ben-Joseph <opensource@phrostbyte.com>
2018-06-14 17:50:09 -0400

    Various changes to the model.

--- tensorspace/model.py
+++ tensorspace/model.py
@@ -1,36 +1,35 @@
 import os
 import sys
+import enum
 
 from pathlib import Path
 from sqlalchemy import (Boolean, Column, Date, Float, ForeignKey, Integer,
                         SmallInteger, String, LargeBinary, create_engine)
+from sqlalchemy.types import Enum
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from tensorspace.util import get_data_home
 
 Base = declarative_base()
 
-def get_config_single_host(query_only=True):
-    home = get_data_home()
-    config = 'sqlite:///' + str(home / 'index.db')
+def get_config_single_host(initialize=False):
+    config = 'sqlite:///' + str(get_data_home() / 'index.db')
     engine = create_engine(config)
-    session = sessionmaker(bind=engine)()
-    if query_only:
-        return session.query
-    else:
+    session = sessionmaker(bind=engine)() 
+    if initialize:
         Base.metadata.create_all(engine)
-        return session
+    return session
 
 class ControlledImport:
-    def __init__(self, name, has_files=False):
+    def __init__(self, session, has_files=False):
         self.home = get_data_home()
         if not self.home.is_dir():
             Path.mkdir(self.home, parents=True)
         if has_files:
-            self.home = self.home / name
+            self.home = self.home / 'images'
             if not self.home.is_dir():
                 Path.mkdir(self.home, parents=True)
-        self.session = get_config_single_host(query_only=False)
+        self.session = session
 
     def exists(self, fname):
         return (self.home / fname).exists()
@@ -39,10 +38,18 @@ class ControlledImport:
         return str(self.home / fname)
 
 
+class ImageFileType(enum.Enum):
+    JPEG = 1
+    PNG = 2
+    GIF = 3
+    TIFF = 4
+    WEBP = 5
+
+
 class Image(Base):
     __tablename__ = 'images'
     id = Column(Integer, primary_key=True)
-    filename = Column(String)
+    filetype = Column(Enum(ImageFileType))
     src_filename = Column(String)
     url = Column(String)
     orig_url = Column(String)
@@ -105,4 +112,5 @@ class Vector(Base):
     __tablename__ = 'vectors'
     id = Column(Integer, primary_key=True)
     caption_id = Column(Integer, ForeignKey('captions.id'), nullable=True)
+    image_id = Column(Integer, ForeignKey('images.id'), nullable=True)
     vec = Column(LargeBinary)
